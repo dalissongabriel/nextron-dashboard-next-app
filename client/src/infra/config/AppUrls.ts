a0dd47e96a6d8f5c8bc01d2c1ff3fc161b3d6ddf
dalissongabriel <alissongabrieldino@gmail.com>
2023-02-27 22:10:02 -0300

    chore(deploy): configure enviroments variables

--- client/src/infra/config/AppUrls.ts
+++ client/src/infra/config/AppUrls.ts
@@ -1,4 +1,4 @@
 export const AppURLs = {
-  next: "http://localhost:3000/api",
-  api: "http://localhost:8081",
+  next: process.env.NEXT_PUBLIC_NODE_API_HOST,
+  api: process.env.NEXT_PUBLIC_BACKEND_API_HOST,
 };
